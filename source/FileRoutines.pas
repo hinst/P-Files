2b3efb8c7c85260186e9c92bd547ffebe3cc5d1c
hinst <hinstance@yandex.ru>
2013-05-09 14:55:14 +0400

    New routine added; writes a string to a stream.

--- source/FileRoutines.pas
+++ source/FileRoutines.pas
@@ -3,9 +3,14 @@ unit FileRoutines;
 interface
 
 uses
-  SysUtils;
+  SysUtils,
+  Classes,
+
+  StringRoutines;
   
 function Path(const s: array of string): string; overload;
+
+function Write(const aStream: TStream; const s: String): boolean; overload;
 
 implementation
 
@@ -22,6 +27,11 @@ begin
     for i := 1 to Length(s) - 1 do
       result := result + PathDelim + s[i];
   end;  
+end;
+
+function Write(const aStream: TStream; const s: String): boolean;
+begin
+  aStream.Write(PChar(s)^, GetByteLength(s));
 end;
 
 end.
